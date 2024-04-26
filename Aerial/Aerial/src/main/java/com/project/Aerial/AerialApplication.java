ee62cc37bfd485d2888c5e699b4d0d1b26efdd2b
MarcosPaAc <isaelpa09@gmail.com>
2024-04-26 02:21:35 -0400

    initial commit

---
+++ Aerial/Aerial/src/main/java/com/project/Aerial/AerialApplication.java
@@ -0,0 +1,19 @@
+package com.project.Aerial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AerialApplication  {
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerialApplication.class, args);
+		
+	}
+
+
+	
+	
+
+}
---
+++ Aerial/Aerial/src/test/java/com/project/Aerial/AerialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Aerial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
