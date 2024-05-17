c026222ad1aed9c780d3e175dbf2c162b804618c
MarcosPaAc <isaelpa09@gmail.com>
2024-05-17 18:16:59 -0400

    aerial htmls files added

--- Aerial/Aerial/src/main/java/com/project/Aerial/FlightController.java
+++ Aerial/Aerial/src/main/java/com/project/Aerial/FlightController.java
@@ -1,11 +1,14 @@
 package com.project.Aerial;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,11 @@ public class FlightController {
 	@GetMapping
 	public ResponseEntity<List<Flights>> getAllFlights() {
 		return new ResponseEntity<List<Flights>>(flightService.allFlights(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Flights>> getSingleFlight(@PathVariable ObjectId id){
+		return new ResponseEntity<Optional<Flights>>(flightService.singleFlight(id), HttpStatus.OK);
 	}
 
 }
